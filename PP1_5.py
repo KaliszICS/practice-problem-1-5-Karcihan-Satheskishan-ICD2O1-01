f4610350c8a98ce2489ac1f01251edb71a55db92
Karcihan-Satheskishan-ICD2O1-01 <karcihan.satheskishan@student.tdsb.on.ca>
2024-09-23 14:19:42 +0000

    k

--- PP1_5.py
+++ PP1_5.py
@@ -5,36 +5,36 @@
     Date Last Modified: Sept 23, 2024
 '''
 def q1():
-  num = input("Enter an integer: ")
+  num = input("Input an integer: ")
   num = int(num)
   num += 3
   print(num)
 
 def q2():
-  num = input("Enter a number: ")
-  num = (num + "4")
+  num = input("Input a number: ")
+  num = num + "4"
   num = float(num)
-  num += 2
+  num = (num + 2)
   print(num)
 
 def q3():
-  rad = input("Enter a radius: ")
+  rad = input("Input a radius: ")
   rad = float(rad)
   area = (3.14 * rad * rad)
   print(area)
 
 def q4():
-  num = input("Enter a number: ")
+  num = input("Input a number: ")
   num = float(num)
   num = (num * 12)
   num = int(num)
   print(num)
 
 def q5():
-  num = input("Enter a number: ")
-  num = int(num)
-  num += 5
-  print(f"Your number + 5 is {num}.")
+  var = input("Input an integer: ")
+  var = int(var)
+  var = (var + 5)
+  print(f"Your number + 5 is {var}.\n")
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
 
